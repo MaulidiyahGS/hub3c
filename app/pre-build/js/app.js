6b8d6fdf88ad5fe7ba854b549dda4dd269e29ba2
Nur Maulidiyah(NYTLYS) <nur.maulidiyah20@gmail.com>
2016-10-25 04:48:21 +0700

    add ng

--- app/pre-build/js/app.js
+++ app/pre-build/js/app.js
@@ -9,22 +9,83 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     	controller: function($scope){
       	$scope.showMenuMobile = false;
         $scope.changeMenu = function(){
-          console.log('click');
           $scope.showMenuMobile = !$scope.showMenuMobile;
-          console.log($scope.showMenuMobile);
         }
+        $scope.menus =  [
+                          {
+                            state : 'landing.home',
+                            text : 'hub3c'
+                          },
+                          {
+                            state : 'landing.app',
+                            text : 'Apps'
+                          },
+                          {
+                            state : 'landing.partner',
+                            text : 'Global Partner Program'
+                          },
+                          {
+                            state : 'landing.about',
+                            text : 'About Us'
+                          }
+                        ];
       }
   	})
 
   	.state('landing.home', {
-	    url: "landing/home",
+	    url: "home",
 	    templateUrl: 'views/parsial/home.html',
-        controller: function($scope){
-        	console.log('home');
-        }
-  	});
-  	$urlRouterProvider.otherwise("landing/home");
+      controller: function($scope){
+      	console.log('home');
+      }
+  	})
+
+    .state('landing.app', {
+      url: "app",
+      templateUrl: 'views/parsial/app.html',
+      controller: function($scope){
+        console.log('app');
+      }
+    })
+
+    .state('landing.partner', {
+      url: "partner",
+      templateUrl: 'views/parsial/partner.html',
+      controller: function($scope){
+        console.log('partner');
+      }
+    })
+
+    .state('landing.about', {
+      url: "about",
+      templateUrl: 'views/parsial/about.html',
+      controller: function($scope){
+        console.log('about');
+      }
+    })
+
+    .state('login', {
+      url: "login",
+      templateUrl: 'views/login.html',
+      controller: function($scope){
+        console.log('login');
+      }
+    });
+  	/*$urlRouterProvider.otherwise(function($injector) {
+      var $state = $injector.get('$state');
+      $state.go('landing.home');
+    });*/
 });
-app.controller('MainCtrl', function ($state) {
-    $state.transitionTo('landing.home');
-})
+/*app.controller('MainCtrl', function ($scope, $state) {
+    $state.go('landing.home');
+    $scope.goTo = function(state){
+      $state.go(state);
+    }
+})*/
+
+app.run(function($rootScope, $state){
+  $state.go('landing.home');
+  $rootScope.goTo = function(state){
+    $state.go(state);
+  }
+});
